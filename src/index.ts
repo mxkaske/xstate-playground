ed2a12cd6e9b59af5dcf7d17d09a7ca610e9edfd
Maximilian Kaske <maximilian@kaske.org>
2023-04-14 12:45:12 +0200

    chore: init commit

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+import { createMachine, interpret } from "xstate";
+import "./styles.css";
+
+const app = document?.getElementById("app");
+
+if (app) {
+  app.innerHTML = `
+<h1>XState TypeScript Example</h1>
+<div>
+  Open the <strong>Console</strong> to view the machine output.
+</div>
+`;
+}
+
+interface ToggleContext {
+  count: number;
+}
+
+type ToggleEvent = {
+  type: "TOGGLE";
+};
+
+// Edit your machine(s) here
+const machine = createMachine<ToggleContext, ToggleEvent>({
+  id: "machine",
+  initial: "inactive",
+  context: {
+    count: 0,
+  },
+  states: {
+    inactive: {
+      on: { TOGGLE: "active" },
+    },
+    active: {
+      on: { TOGGLE: "inactive" },
+    },
+  },
+});
+
+// Edit your service(s) here
+const service = interpret(machine).onTransition((state) => {
+  console.log(state.value);
+});
+
+service.start();
+
+service.send("TOGGLE");
+service.send("TOGGLE");
+
+console.log("hello world");
